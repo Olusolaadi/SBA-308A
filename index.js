fccc63083dfbda4fc66ecebcf33f0bd97700dd91
Olusolaadi <olusolafatunde@gmail.com>
2024-09-17 20:08:01 -0500

    SBA 308 A

---
+++ index.js
@@ -0,0 +1,230 @@
+// Import the api key. 
+import { API_KEY } from "./key.js";
+
+// The 'currencies' object
+const currencies = {
+    USD: "US",
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+}
+
+// Fetch api
+async function getCurrencyRate() {
+
+        const res = await fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`);
+
+       console.log(res);
+    
+        const json = await res.json();
+        return json;
+    
+    }
+    console.log(getCurrencyRate());
+
+    // Fetch codes
+    async function getCurrencyCodes() {
+    
+    const res = await fetch (`https://v6.exchangerate-api.com/v6/${API_KEY}/codes`);
+
+
+        const json = await res.json();
+        return json;
+    }
+    console.log(getCurrencyCodes());
+
+    const currentRate = document.getElementById("current-rate");
+    const convert = document.getElementById("convert");
+    const from = document.getElementById("from");
+    const to = document.getElementById("to");
+    const amount = document.getElementById("amount");
+    convert.addEventListener("click", function() {
+       let fCurr = from.value;
+       let tCurr = to.value;
+       let val_amt = amount.value;
+
+       fetch(`https://api.exchangerate-api.com/v6/latest`)
+       .then(res => {
+             return res.json();
+       })
+       .then(data => {
+          const rate = data.rates[tCurr];
+          const cal_Rate = rate * val_amt;
+          currentRate.innerHTML = `${val_amt} ${fCurr} = ${cal_Rate} ${tCurr}`;
+       });
+    });
+
+    //Generate options of currencies for from and to 
+
+const genOpt = listCurr(currencies);
+console.log(genOpt);
+
+// Loop over the object 'currencies' and add 'option' element
+function listCurr(options) {
+  return Object.entries(options)
+    .map(([currencyKeys, currencyCode]) =>
+        `<option value="${currencyKeys}">${currencyKeys} - ${currencyCode}</option>`)
+    .join("");
+}
+
+const fromOpt = document.getElementById('from');
+const toOpt = document.getElementById('to');
+
+fromOpt.innerHTML = genOpt;
+toOpt.innerHTML = genOpt;
